b4706d49061d826c616c0f68818ba75840f09c2b
srikar <ventrapragadasaisrikar123@gmail.com>
2022-11-21 13:09:42 +0530

    Stats Update

--- app.py
+++ app.py
@@ -10,13 +10,31 @@ with st.container():
 
 with st.container():
      st.header("Target Market")
-     st.write("As you can see the target market for our product would be all the Participants for the various events that take place the exact number comes down to ")
+     percent = 96
+     st.write("As you can see the target market for our product would be all the Participants for the various events that take place the exact-: ")
+     st.write(f"The number comes down to {percent}% of the meta data")
      
      
 with st.container():
      st.header("Market Size")
-     st.write("As you can see from the pie chart our market size is  %")
-     
+     img = Image.open("Images/roles.png")
+     st.image(img,width=500) 
+     st.write("As you can see from the pie chart our market size is about 96%") 
+
+with st.container():
+     st.header("Market Segmentation")
+     st.write("The market can be divided in 2 ways-:")
+     st.write("The First being with respect to different year Students and Later being with respect to Clubs")
+     col1, col2= st.columns([1,1]) 
+with col1:
+     img = Image.open("Images/students.png")
+     st.image(img,width=500) 
+     st.write("We can infer from the given data that from clubs we can see that they are more 17XJ than 18XJ in terms of participatents thereby giving us a clear indication on whom to target")
+
+with col2:
+     img = Image.open("Images/clubs.png")
+     st.image(img,width=500) 
+     st.write("As we can see the CLubs Bar Graph they are almost quite evenly distributed thereby tell us that each club has equal amount of potential") 
 
 
 with st.container():
@@ -34,13 +52,34 @@ with st.container():
      col1, col2= st.columns([1,1])
 
 with col1:
-     img = Image.open("fest1.png")
+     img = Image.open("Images/fest1.png")
      st.image(img,width=500) 
      st.write("")
 
 with col2:
-     img = Image.open("fest2.png")
-     st.image(img,width=500)   
+     img = Image.open("Images/fest2.png")
+     st.image(img,width=500) 
+     st.write("") 
+
+with st.container(): 
+          col1, col2,col3= st.columns([1,1,1])
+
+with col1:
+     img = Image.open("Images/Club1.png")
+     st.image(img,width=500) 
+     st.write("")
+
+with col2:
+     img = Image.open("Images/Club2.png")
+     st.image(img,width=500) 
+     st.write("") 
+
+
+with col3:
+     img = Image.open("Images/Club3.png")
+     st.image(img,width=500) 
+     st.write("") 
+
 
 with st.container(): 
      st.write("[For More information click here](https://github.com/Baka-14/MindGraph-Hackathon)")
